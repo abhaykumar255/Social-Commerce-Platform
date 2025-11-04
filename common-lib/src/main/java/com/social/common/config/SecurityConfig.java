518d1b1cecd34036709c5912e608e16e71e674cd
Abhay Kumar <rajputak255@gmail.com>
2025-11-04 14:50:24 +0530

    updating common lib and root pom.xml

--- common-lib/src/main/java/com/social/common/config/SecurityConfig.java
+++ common-lib/src/main/java/com/social/common/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.social.gateway.config;
+package com.social.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
