2ff08e2127a9205688fcc131405a06ec6c8c99ca
Abhay Kumar <rajputak255@gmail.com>
2025-11-10 23:17:09 +0530

    updating Permissions and Roles also

--- user-service/src/main/java/com/social/user/model/Permission.java
+++ user-service/src/main/java/com/social/user/model/Permission.java
@@ -1,4 +1,31 @@
 package com.social.user.model;
 
-public class Permission {
+import com.social.common.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "permissions")
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Permission extends BaseEntity {
+
+    @Column(name = "name", nullable = false, unique = true, length = 100)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "resource", nullable = false, length = 50)
+    private String resource;
+
+    @Column(name = "action", nullable = false, length = 50)
+    private String action;
 }
--- user-service/src/main/java/com/social/user/model/Role.java
+++ user-service/src/main/java/com/social/user/model/Role.java
@@ -1,4 +1,35 @@
 package com.social.user.model;
 
-public class Role {
+import com.social.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role extends BaseEntity {
+
+    @Column(name = "name", nullable = false, unique = true, length = 50)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "role_permissions",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "permission_id")
+    )
+    private Set<Permission> permissions = new HashSet<>();
+
 }
