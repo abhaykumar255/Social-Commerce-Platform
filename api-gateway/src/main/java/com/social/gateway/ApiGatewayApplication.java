7746bb4d97abf5e313c2f56bb961b1af0c6d4e7e
Abhay Kumar <rajputak255@gmail.com>
2025-11-17 20:57:37 +0530

    fe:adding the api-gateway configuration

--- api-gateway/src/main/java/com/social/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/social/gateway/ApiGatewayApplication.java
@@ -1,4 +1,16 @@
 package com.social.gateway;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan(basePackages = {"com.social.gateway", "com.social.common"})
 public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 }
